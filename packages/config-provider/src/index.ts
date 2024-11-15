dd6bdca64602dc88be1438b252662d4d1bbd3ef7
Uros Zagoranski <urozag@gmail.com>
2024-11-15 12:53:43 +0100

    Add exports to index.js as entry point of the package

---
+++ packages/config-provider/src/index.ts
@@ -0,0 +1,5 @@
+export * from "./contracts/interfaces/IConfigProvider";
+export * from "./exceptions/ConfigException";
+export * from "./providers/NconfConfigProvider";
+export * from "./providers/PredefinedConfigProvider";
+export * from "./ConfigProviderModule";
